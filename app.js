3cbfb1994ffe9026b2b130fec95a649e77c11be3
Jørgen Borup <jorgen.borup@gmail.com>
2023-01-15 01:08:24 +0100

    A

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const core = require('@actions/core')
+// const core = require('@actions/core')
 
 async function run() {
     try {
-        const test = core.getInput('test');
-        core.debug("Function ran...");
+//        const test = core.getInput('test');
+//        core.debug("Function ran...");
         console.log('Hello World!');
     }
     catch (error) {
-        core.setFailed("I FAILED");
+//        core.setFailed("I FAILED");
     }
 }
 
